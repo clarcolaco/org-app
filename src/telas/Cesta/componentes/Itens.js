a22dcc248f7023ceecc570634894a9e1ad2e174c
Clarissa Colaco <clarissa.colaco@hotmail.com>
2021-09-18 18:45:28 -0300

    lista finalizada com map

--- src/telas/Cesta/componentes/Itens.js
+++ src/telas/Cesta/componentes/Itens.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, View, StyleSheet } from "react-native";
 import Texto from "../../../componentes/Texto";
 
 
@@ -7,16 +7,18 @@ export default function Itens({ tituloItens, lista }) {
 
     return <>
 
-        <Texto>
+
+        <Texto style={estilos.titulo}>
             {tituloItens}
         </Texto>
 
 
         {lista.map(({ nome, imagem }) => {
 
-            return <View key={nome}>
-                <Texto >{nome}</Texto>
-                <Image source={imagem} />
+            return <View key={nome} style={estilos.item}>
+                <Image source={imagem} style={estilos.imagem} />
+                <Texto style={estilos.texto}>{nome}</Texto>
+                
             </View>
         })
         }
@@ -30,3 +32,37 @@ export default function Itens({ tituloItens, lista }) {
 
 
 
+const estilos = StyleSheet.create({
+
+    titulo: {
+        color: "#464646",
+        fontWeight: "bold",
+        marginTop: 12,
+        marginBottom: 8,
+        fontSize: 20,
+        lineHeight: 32,
+    },
+    item: {
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        borderBottomWidth: 1,
+        borderBottomColor: "#ececec",
+        paddingVertical:16,
+        alignItems: "center",
+        
+    },
+    imagem: {
+    width:46,
+    height: 46,
+    
+    
+    },
+    texto:{
+        marginLeft: 10,
+        fontSize: 16,
+        lineHeight:26,
+        color: "#464646",
+        
+    }
+
+});
